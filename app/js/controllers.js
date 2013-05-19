5ac9466375d3d9c1bd5ebc6ace4ba445e6ab1714
Hurricane Hamilton <hhamilto@mtu.edu>
2013-05-19 19:24:20 -0400

    added some image files

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,33 +4,24 @@
 
 
 function Milestones($scope, $timeout){
+    // the 70s
+    var time = 0;
+    $scope.working = true;
+    
     $scope.milestones = [
         {milestoneText:"Finish the app", timeDebt: 900, timePaid: 90, timeCost: null, done: false},
         {milestoneText:"Go to Bed",  timeDebt: 900, timePaid: 0, timeCost: null, done: false}
     ];
     
-    $scope.working = true;
     
     $scope.num_milestones = function(){
-        $scope.milestones.length();
+        $scope.milestones.length;
     };
     
-    function toggleDone(milestone){
-        milestone.done = !milestone.done;
-    }
-    
-    function buttonText(milestone){
-        if(milestone.done){
-            return 'JK';
-        }else{
-            return 'DONE';
-        }
-    }
-    
     $timeout(function someWork(){
-        $scope.time = Math.round(new Date().getTime() / 1000);
-        for(var i = 0; i < $scope.milestones.length() &&  $scope.working; i++){
-            $scope.milestones[i].timePaid++;
+        time = Math.round(new Date().getTime() / 1000);
+        if($scope.working){
+            $scope.milestones[0].timePaid++;
         }
         $timeout(someWork, 1000);
     },1000);
@@ -38,6 +29,40 @@ function Milestones($scope, $timeout){
     $scope.addMilestone = function(arg){
         alert(arg);
     };
+    
+    $scope.timeText = function(milestone){
+        var index = $scope.milestones.indexOf(milestone);
+        // if its the first one, then treat it spectial
+        if(index === 0){
+            return milestone.timeDebt - milestone.timePaid+ ' s';
+        }else if( index > 0){
+            return milestone.timeDebt - milestone.timePaid+ ' s';
+        }else{
+            console.log("ERROR: pass a mile stone in.");
+        }
+        alert(milestone);
+    };
+    
+    $scope.icon = function(milestone){
+        if(milestone.done){
+            return 'icon-arrow-left';
+        }else{
+            return 'icon-ok';
+        }
+    };
+    
+    $scope.toggleDone = function(milestone){
+        milestone.done = !milestone.done;
+    };
+    
+    $scope.buttonText = function(milestone){
+        if(milestone.done){
+            return 'JK';
+        }else{
+            return 'DONE';
+        }
+    };
+    
 }
 
 function MyCtrl2() {
